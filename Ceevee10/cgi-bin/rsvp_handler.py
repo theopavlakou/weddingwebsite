24a6f8631dff099f80e6d198ff7936c090f3e7a5
Theo Pavlakou <theo.pavlakou@gmail.com>
2014-07-28 12:53:04 +0300

    Now uses regex to check email

--- Ceevee10/cgi-bin/rsvp_handler.py
+++ Ceevee10/cgi-bin/rsvp_handler.py
@@ -9,6 +9,8 @@ print(yate.include_head("Some title"))
 print(yate.start_body())
 firstName = formData["firstname"].value
 surname = formData["surname"].value
+# TODO Must check the information actually is valid (again, server side)
+# and must ensure that they are not already in the system.
 print(yate.include_section("twelve columns", "We'll see you there!", "You have now joined the guest list " + firstName + " " + surname + "!" + '<br><br><a class="button" href="../index.html">Go back</a>'))
 print(yate.end_body())
 print(yate.include_footer())
